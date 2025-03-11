ac72a07ad19fae13fb3be191b80cd12e8961a396
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2025-03-11 08:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
-
+document.querySelectorAll("strong").forEach((element) => {
+        element.style.color = "rgb(0, 128, 0)"; // Change color to green
+    });
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+ document.querySelectorAll("strong").forEach((element) => {
+        element.style.color = "rgb(0, 0, 0)"; // Revert color to black
+    });
     
 }
